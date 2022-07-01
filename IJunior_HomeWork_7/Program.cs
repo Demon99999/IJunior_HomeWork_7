0dec6ea7ed587a09c675f6af88f7c39967a8152f
Demon99999 <35568272+Demon99999@users.noreply.github.com>
2022-07-01 17:22:57 +0300

    First commit

---
+++ IJunior_HomeWork_7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IJunior_HomeWork_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountGold;
+            int countCrystals;
+            int priceCrystal = 50;
+            Console.WriteLine("Добро пожаловать в магазин кристаллов!!!");
+            Console.WriteLine("Сколько у вас золота ?");
+            amountGold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Сколько кристаллов вы хотите приобрести ?");
+            countCrystals = Convert.ToInt32(Console.ReadLine());
+            amountGold -= (countCrystals * priceCrystal);
+            Console.WriteLine($"Количество кристаллов: {countCrystals} . Остаток золота: {amountGold}. ");
+        }
+    }
+}
